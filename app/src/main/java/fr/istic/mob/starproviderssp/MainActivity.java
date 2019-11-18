f35c0f697a9037202f0be472813cb31c72687ecc
Arthur Palone <ws.palone@gmail.com>
2019-11-18 16:34:14 +0100

    test archive pour quentin

--- app/src/main/java/fr/istic/mob/starproviderssp/MainActivity.java
+++ app/src/main/java/fr/istic/mob/starproviderssp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //test pour quentin
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
