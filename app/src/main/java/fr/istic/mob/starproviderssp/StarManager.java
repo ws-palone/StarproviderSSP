c2234cd5f7a3037efb3d93ceb602d55ba115d9a3
Sarrazin Quentin <quentin.sarrazin@etudiant.univ-rennes1.fr>
2019-11-27 13:10:46 +0100

    Recup json en string

--- app/src/main/java/fr/istic/mob/starproviderssp/StarManager.java
+++ app/src/main/java/fr/istic/mob/starproviderssp/StarManager.java
@@ -7,9 +7,15 @@ import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -26,18 +32,40 @@ public class StarManager extends Worker {
             String myUrl = "https://data.explore.star.fr/explore/dataset/tco-busmetro-horaires-gtfs-versions-td/download/?format=json&timezone=Europe/Berlin";
             URL url = new URL(myUrl);
             HttpURLConnection connexion = (HttpURLConnection) url.openConnection();
-            connexion.setRequestMethod("GET");
-            connexion.connect();
-            InputStream inputStream = connexion.getInputStream();
+            try {
+                InputStream in = new BufferedInputStream(connexion.getInputStream());
+                readStream(in);
+            } finally {
+                connexion.disconnect();
+            }
+            /*InputStream inputStream = connexion.getInputStream();
             String result = inputStream.toString();
             JSONObject jsonObject = new JSONObject(result);
-            JSONObject fields = jsonObject.getJSONObject("fields");
-            JSONObject urljson = fields.getJSONObject("url");
-            String urlbase = urljson.toString();
-            Log.d("bonjour",urlbase);
+            JSONArray fields = jsonObject.getJSONArray("fields");
+            for (int i=0; i < fields.length(); i++)
+            {
+                try {
+                    JSONObject oneObject = fields.getJSONObject(i);
+                    // Pulling items from the array
+                    String urlbase = oneObject.toString();
+                    Log.d("bonjour",urlbase);
+                } catch (JSONException e) {
+                }
+            }*/
         }catch (Exception e){
             e.printStackTrace();
         }
         return Result.SUCCESS;
+    }
+
+    private String readStream(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
+        for (String line = r.readLine(); line != null; line =r.readLine()){
+            sb.append(line);
+        }
+        is.close();
+        Log.d("bonjour",sb.toString());
+        return sb.toString();
     }
 }
