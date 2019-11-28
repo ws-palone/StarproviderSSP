46a645b0a966a6fd00d5bb520ec56657a879dc13
Arthur Palone <ws.palone@gmail.com>
2019-11-28 11:53:09 +0100

    ajout spinner + passage version 15 to 23

--- app/src/main/java/fr/istic/mob/starproviderssp/MainActivity.java
+++ app/src/main/java/fr/istic/mob/starproviderssp/MainActivity.java
@@ -5,6 +5,10 @@ import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
@@ -45,6 +49,20 @@ public class MainActivity extends AppCompatActivity {
                 .setProgress(0, 0, false);
         notificationManager.notify(1, builder.build());
 
+        Button pickDateHour = findViewById(R.id.pick_date_hour);
+        pickDateHour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePicker datePicker = findViewById(R.id.datePicker);
+                int month = datePicker.getMonth()+1;
+                int day = datePicker.getDayOfMonth();
+                int year = datePicker.getYear();
+                TimePicker timePicker = findViewById(R.id.timePicker);
+                int hour = timePicker.getHour();
+                int minute = timePicker.getMinute();
+                //Toast.makeText(MainActivity.this, timeToString(hour,minute), Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
@@ -90,6 +108,43 @@ public class MainActivity extends AppCompatActivity {
         OneTimeWorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(StarManager.class).build();
         WorkManager.getInstance().enqueue(uploadWorkRequest);
     }
+
+    public String dateToString(int month, int day, int year){
+        String monthToString, daytoString;
+        if (month<10){
+            monthToString = "0"+month;
+        }
+        else{
+            monthToString = ""+month;
+        }
+        if (day<10){
+            daytoString = "0"+day;
+        }
+        else{
+            daytoString = ""+day;
+        }
+        String date = year+monthToString+daytoString;
+        return date;
+    }
+
+    public String timeToString (int hour, int minute){
+        String hourToString, minutetoString;
+        if (hour<10){
+            hourToString= "0"+hour;
+        }
+        else{
+            hourToString = ""+hour;
+        }
+        if (minute<10){
+            minutetoString = "0"+minute;
+        }
+        else{
+            minutetoString = ""+minute;
+        }
+        String time = hourToString+":"+minutetoString;
+        return time;
+    }
+}
 
 
 }
